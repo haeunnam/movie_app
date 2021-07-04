6f3ba47218db517ae86a768ef077008ea3ebd557
multicampus <plain3785@gmail.com>
2021-07-04 15:26:05 +0900

    finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -11,18 +12,33 @@ class App extends React.Component {
     const { data: { data: { movies } } } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating")
     this.setState({ movies, isLoading: false })
   }
-  componentDidMount() {
+  componentDidMount() { 
     this.getMovie()
   }
   render() {
     const { isLoading, movies } = this.state
     return (
-      <div>
-        {isLoading ? "Loading.."
-          : movies.map(movie => (
-            <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
+      <section className="container">
+        {isLoading ? (
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div>
+         ) : (
+          <div className="movies">
+            {movies.map(movie => ( 
+            <Movie 
+              key={movie.id} 
+              id={movie.id}
+              year={movie.year} 
+              title={movie.title} 
+              summary={movie.summary} 
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+              />
           ))}
-      </div>
+          </div>
+         )}
+      </section>
     )
   }
 }
--- src/Potato.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Potato(){
-  return <h3>potato is here</h3>
-}
-
-export default Potato;
